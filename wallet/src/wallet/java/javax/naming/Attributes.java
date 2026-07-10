e43721e035ac33d7e64ca556e0a48c8f0ce61b61
buli <hvlrcb@gmail.com>
2026-07-10 13:44:42 +0700

    Delete wallet/src/wallet/java/javax/naming/Attributes.java

--- wallet/src/wallet/java/javax/naming/Attributes.java
+++
@@ -1,6 +0,0 @@
-package javax.naming.directory;
-import javax.naming.NamingEnumeration;
-public interface Attributes {
-    Attribute get(String attrID);
-    NamingEnumeration<? extends Attribute> getAll();
-}
