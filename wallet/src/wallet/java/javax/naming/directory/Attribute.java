a568d094ff34f0bbca11a5dffa904310d03d735a
buli <hvlrcb@gmail.com>
2026-07-10 15:05:57 +0700

    Add size and getAll methods to Attribute interface

--- wallet/src/wallet/java/javax/naming/directory/Attribute.java
+++ wallet/src/wallet/java/javax/naming/directory/Attribute.java
@@ -1,6 +1,9 @@
 package javax.naming.directory;
 import javax.naming.NamingException;
+import javax.naming.NamingEnumeration;
 public interface Attribute extends Cloneable, java.io.Serializable {
     String getID();
     Object get() throws NamingException;
+    int size();
+    NamingEnumeration<?> getAll() throws NamingException;
 }
