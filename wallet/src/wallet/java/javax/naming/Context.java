6386766f616a1aeacf004bcda9aed9e834037671
buli <hvlrcb@gmail.com>
2026-07-10 15:04:29 +0700

    Update Context.java

--- wallet/src/wallet/java/javax/naming/Context.java
+++ wallet/src/wallet/java/javax/naming/Context.java
@@ -1,4 +1,19 @@
 package javax.naming;
 public interface Context {
+    String INITIAL_CONTEXT_FACTORY = "java.naming.factory.initial";
+    String OBJECT_FACTORIES = "java.naming.factory.object";
+    String STATE_FACTORIES = "java.naming.factory.state";
+    String URL_PKG_PREFIXES = "java.naming.factory.url.pkgs";
+    String PROVIDER_URL = "java.naming.provider.url";
+    String DNS_URL = "java.naming.dns.url";
+    String AUTHORITATIVE = "java.naming.authoritative";
+    String BATCHSIZE = "java.naming.batchsize";
+    String REFERRAL = "java.naming.referral";
+    String SECURITY_PROTOCOL = "java.naming.security.protocol";
+    String SECURITY_AUTHENTICATION = "java.naming.security.authentication";
+    String SECURITY_PRINCIPAL = "java.naming.security.principal";
+    String SECURITY_CREDENTIALS = "java.naming.security.credentials";
+    String LANGUAGE = "java.naming.language";
+    String APPLET = "java.naming.applet";
     void close() throws NamingException;
 }
