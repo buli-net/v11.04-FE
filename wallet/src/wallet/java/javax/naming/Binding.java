8cb6c7be35bcef919f182956d74a4bd569f330b7
buli <hvlrcb@gmail.com>
2026-07-10 15:47:26 +0700

    Update Binding.java

--- wallet/src/wallet/java/javax/naming/Binding.java
+++ wallet/src/wallet/java/javax/naming/Binding.java
@@ -1,10 +1,6 @@
 package javax.naming;
-import java.io.Serializable;
-public class Binding implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private String name;
+public class Binding extends NameClassPair {
     private Object obj;
-    public Binding(String name, Object obj) { this.name = name; this.obj = obj; }
-    public String getName() { return name; }
-    public Object getObject() { return obj; }
+    public Binding(String n,Object o){super(n,o!=null?o.getClass().getName():null);obj=o;}
+    public Object getObject(){return obj;}
 }
