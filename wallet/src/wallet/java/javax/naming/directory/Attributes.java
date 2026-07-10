c70253e5ee4a8651973071047315fc2ace7a6a8b
buli <hvlrcb@gmail.com>
2026-07-10 15:05:13 +0700

    Update Attributes.java

--- wallet/src/wallet/java/javax/naming/directory/Attributes.java
+++ wallet/src/wallet/java/javax/naming/directory/Attributes.java
@@ -1,4 +1,8 @@
 package javax.naming.directory;
+import javax.naming.NamingEnumeration;
 public interface Attributes extends Cloneable, java.io.Serializable {
     Attribute get(String attrID);
+    NamingEnumeration<? extends Attribute> getAll();
+    NamingEnumeration<String> getIDs();
+    int size();
 }
