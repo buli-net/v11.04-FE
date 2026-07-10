2cae10f8ee7065e5ca36cbcbc1c5491082b70ea3
buli <hvlrcb@gmail.com>
2026-07-10 09:50:09 +0700

    Change DistributionPoint parameter to Object in getCrl

--- wallet/src/wallet/java/org/bouncycastle/jce/provider/CrlCache.java
+++ wallet/src/wallet/java/org/bouncycastle/jce/provider/CrlCache.java
@@ -2,17 +2,16 @@ package org.bouncycastle.jce.provider;
 
 import java.security.cert.CertificateFactory;
 import java.util.Date;
-import org.bouncycastle.asn1.x509.DistributionPoint;
 import org.bouncycastle.jcajce.PKIXCRLStore;
 
 public class CrlCache
 {
-    public static PKIXCRLStore getCrl(CertificateFactory certFact, Date date, DistributionPoint dp)
+    public static PKIXCRLStore getCrl(CertificateFactory certFact, Date date, Object crl)
     {
         return null;
     }
 
-    public static PKIXCRLStore getCrl(CertificateFactory certFact, Date date, DistributionPoint dp, boolean delta)
+    public static PKIXCRLStore getCrl(CertificateFactory certFact, Date date, Object crl, boolean delta)
     {
         return null;
     }
