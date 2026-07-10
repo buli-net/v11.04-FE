826eceb61303a70103168bcaffff193fc624905f
buli <hvlrcb@gmail.com>
2026-07-10 13:45:07 +0700

    Delete wallet/src/wallet/java/javax/naming/DirContext.java

--- wallet/src/wallet/java/javax/naming/DirContext.java
+++
@@ -1,8 +0,0 @@
-package javax.naming.directory;
-import javax.naming.Context;
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
-public interface DirContext extends Context {
-    Attributes getAttributes(String name) throws NamingException;
-    NamingEnumeration<SearchResult> search(String name, String filter, SearchControls cons) throws NamingException;
-}
