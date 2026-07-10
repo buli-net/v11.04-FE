2d854c495d8702655c3de125708b5037c9ba70cb
buli <hvlrcb@gmail.com>
2026-07-10 13:46:14 +0700

    Delete wallet/src/wallet/java/javax/naming/SearchControls.java

--- wallet/src/wallet/java/javax/naming/SearchControls.java
+++
@@ -1,9 +0,0 @@
-package javax.naming.directory;
-public class SearchControls {
-    public static final int OBJECT_SCOPE = 0;
-    public static final int ONELEVEL_SCOPE = 1;
-    public static final int SUBTREE_SCOPE = 2;
-    public void setSearchScope(int s) {}
-    public void setReturningAttributes(String[] a) {}
-    public void setCountLimit(long l) {}
-}
