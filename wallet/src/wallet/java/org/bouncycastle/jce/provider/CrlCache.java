cf2a7ed3394938acc74f28ac4c6306d99101eab1
buli <hvlrcb@gmail.com>
2026-07-10 09:46:43 +0700

    Update CrlCache.java

--- wallet/src/wallet/java/org/bouncycastle/jce/provider/CrlCache.java
+++ wallet/src/wallet/java/org/bouncycastle/jce/provider/CrlCache.java
@@ -3,12 +3,12 @@ package org.bouncycastle.jce.provider;
 import java.security.cert.CertificateFactory;
 import java.util.Date;
 import org.bouncycastle.asn1.x509.DistributionPoint;
+import org.bouncycastle.jcajce.PKIXCRLStore;
 
 public class CrlCache
 {
     public static PKIXCRLStore getCrl(CertificateFactory certFact, Date date, DistributionPoint dp)
     {
-        // Wallet không dùng CRL LDAP, trả về null để bỏ qua cache
         return null;
     }
 
